6b9b958c2bfc8dece107b1f09a3fe43f1649f71d
Guy Bedford <guybedford@gmail.com>
2019-02-17 03:30:15 +0200

    remove core resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import jspmPlugin from 'rollup-plugin-jspm';
 
-export default [{
+export default {
   input: ['build/@empty.js', 'build/@empty.dew.js', 'assert', 'buffer', 'console', 'constants', 'crypto', 'domain', 'events', 'http', 'https', 'os', 'path', 'process', 'build/process-production.js', 'punycode', 'querystring', 'stream', 'string_decoder', 'sys', 'timers', 'tty', 'url', 'util', 'vm', 'zlib'],
   output: {
     dir: 'nodelibs',
@@ -9,13 +9,4 @@ export default [{
   plugins: [jspmPlugin({
     env: { browser: true }
   })]
-}, {
-  input: ['@jspm/jspm-resolve/resolve.js'],
-  output: {
-    dir: '.',
-    format: 'esm'
-  },
-  plugins: [jspmPlugin({
-    env: { node: true }
-  })]
-}];
+};
