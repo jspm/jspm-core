a9d02b049a7c8db2adf431afa376570c54fa3632
Guy Bedford <guybedford@gmail.com>
2020-04-23 16:40:17 -0700

    2.0.0-beta.5

---
+++ nodelibs/dgram.js
@@ -0,0 +1,2 @@
+function unimplemented(){throw new Error("Node.js dgram module is not supported by jspm core"+("undefined"!=typeof Deno?". Deno support here is tracking in https://github.com/jspm/jspm-core/issues/4, +1's are appreciated!":" in the browser"))}var dgram = {_createSocketHandle:unimplemented,createSocket:unimplemented,Socket:unimplemented};
+export default dgram;export{unimplemented as Socket,unimplemented as _createSocketHandle,unimplemented as createSocket};
