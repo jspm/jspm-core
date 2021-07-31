8f5bd7f9076d9d17395beaa4d14e2ca9777ac5c1
Guy Bedford <guybedford@gmail.com>
2021-07-31 16:39:01 -0700

    feat: add module.ts, deno std v103

--- nodelibs/deno/assert.ts
+++ nodelibs/deno/assert.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.100.0/node/assert.ts';
-export { default } from 'https://deno.land/std@0.100.0/node/assert.ts';
+export * from 'https://deno.land/std@0.103.0/node/assert.ts';
+export { default } from 'https://deno.land/std@0.103.0/node/assert.ts';
