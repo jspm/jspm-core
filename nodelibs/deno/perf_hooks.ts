4148e706e58152fcd1456fdd178c35b1fd4862b7
Jaya Krishna Namburu <namburu1995@gmail.com>
2021-08-03 02:38:13 +0530

    Adding shims for perf_hooks (#19)
    * Adding shims for perf_hooks
    
    * Adding shims for mark and measure and resolveing the PR comments
    
    * Method name and import resolution for perf_hooks

---
+++ nodelibs/deno/perf_hooks.ts
@@ -0,0 +1,3 @@
+export * from 'https://deno.land/std@0.103.0/node/perf_hooks.ts'
+import { default as performance } from 'https://deno.land/std@0.103.0/node/perf_hooks.ts'
+export default performance
---
+++ nodelibs/node/perf_hooks.js
@@ -0,0 +1,2 @@
+export * from "node:perf_hooks";
+export { default } from "node:perf_hooks";
