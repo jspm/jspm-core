3a7a2a69d6f2c9ddbe7bc863ede4fa403c08d0a7
Mestery <mestery@pm.me>
2021-08-18 15:44:27 +0200

    fix deno's buffer nodelib typing issues

--- nodelibs/deno/buffer.ts
+++ nodelibs/deno/buffer.ts
@@ -1,5 +1,8 @@
 export * from 'https://deno.land/std@0.103.0/node/buffer.ts';
 import { default as buffer } from 'https://deno.land/std@0.103.0/node/buffer.ts';
 export const kMaxLength = 4294967296;
+declare module 'https://deno.land/std@0.103.0/node/buffer.ts' {
+  export var kMaxLength: number;
+}
 buffer.kMaxLength = kMaxLength;
 export default buffer;
