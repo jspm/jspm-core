52cbaf6ff8c277ac57c8945827ddd9f20eae130a
Mestery <mestery@pm.me>
2021-08-21 19:40:59 +0200

    feat: web crypto (#24)

--- nodelibs/deno/crypto.ts
+++ nodelibs/deno/crypto.ts
@@ -1,2 +1,8 @@
+// TODO: update this when deno_std/node will export webcrypto (when Node.js Web Crypto API is stable)
 export * from 'https://deno.land/std@0.103.0/node/crypto.ts';
-export { default } from 'https://deno.land/std@0.103.0/node/crypto.ts';
+export const webcrypto = globalThis.crypto;
+import crypto from 'https://deno.land/std@0.103.0/node/crypto.ts';
+export default {
+  ...crypto,
+  webcrypto: globalThis.crypto,
+} as typeof crypto & { webcrypto: typeof webcrypto };
