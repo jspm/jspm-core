b32fcd2933be671d77c22c712b748f0583fb29a9
Guy Bedford <guybedford@gmail.com>
2021-08-28 10:48:22 -0700

    fixup test

--- test/wasi/wasi.mjs
+++ test/wasi/wasi.mjs
@@ -1,14 +1,13 @@
-import fs from '../../nodelibs/browser/fs.js';
+import { readFile } from '../../nodelibs/browser/fs/promises.js';
 import { WASI } from '../../nodelibs/browser/wasi.js';
-import { argv, env } from '../../nodelibs/browser/process.js';
 
 const wasi = new WASI({
   args: ['cowsay', 'moo']
 });
+
 const importObject = { wasi_unstable: wasi.wasiImport };
 
-// QUESTION:: how to ensure fs read to a URL, returns the fetched URL source?
-const wasm = await WebAssembly.compile(fs.readFileSync(new URL('./cowsay.wasm', import.meta.url)));
+const wasm = await WebAssembly.compile(await readFile(new URL('./cowsay.wasm', import.meta.url)));
 const instance = await WebAssembly.instantiate(wasm, importObject);
 
 wasi.start(instance);
