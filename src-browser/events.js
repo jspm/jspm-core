46a981454e502998f14021f0f65018c0d19c3737
Mestery <mestery@pm.me>
2021-08-28 03:22:58 +0200

    feat(browser): events.on (#40)

--- src-browser/events.js
+++ src-browser/events.js
@@ -1,6 +1,82 @@
 import events from 'events';
+// https://github.com/denoland/deno_std/blob/d005433c709054af87aca54f57a446b4f7966f11/node/events.ts#L542
+events.on = function (emitter, event) {
+  const unconsumedEventValues = [];
+  const unconsumedPromises = [];
+  let error = null;
+  let finished = false;
+
+  const iterator = {
+    async next() {
+      const value = unconsumedEventValues.shift();
+      if (value) {
+        return createIterResult(value, false);
+      }
+
+      if (error) {
+        const p = Promise.reject(error);
+        error = null;
+        return p;
+      }
+
+      if (finished) {
+        return createIterResult(undefined, true);
+      }
+
+      return new Promise((resolve, reject) => unconsumedPromises.push({ resolve, reject }));
+    },
+    async return() {
+      emitter.removeListener(event, eventHandler);
+      emitter.removeListener('error', errorHandler);
+      finished = true;
+
+      for (const promise of unconsumedPromises) {
+        promise.resolve(createIterResult(undefined, true));
+      }
+
+      return createIterResult(undefined, true);
+    },
+    throw(err) {
+      error = err;
+      emitter.removeListener(event, eventHandler);
+      emitter.removeListener('error', errorHandler);
+    },
+    [Symbol.asyncIterator]() {
+      return this;
+    },
+  };
+
+  emitter.on(event, eventHandler);
+  emitter.on('error', errorHandler);
+
+  return iterator;
+
+  function eventHandler(...args) {
+    const promise = unconsumedPromises.shift();
+    if (promise) {
+      promise.resolve(createIterResult(args, false));
+    } else {
+      unconsumedEventValues.push(args);
+    }
+  }
+
+  function errorHandler(err) {
+    finished = true;
+    const toError = unconsumedPromises.shift();
+    if (toError) {
+      toError.reject(err);
+    } else {
+      error = err;
+    }
+    iterator.return();
+  }
+}
 export default events;
-export var EventEmitter = events.EventEmitter;
-export var defaultMaxListeners = events.defaultMaxListeners;
-export var init = events.init;
-export var listenerCount = events.listenerCount;
+export const {
+  EventEmitter,
+  defaultMaxListeners,
+  init,
+  listenerCount,
+  on,
+  once,
+} = events;
