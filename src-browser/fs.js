98fba6a5465a93f0029c8d0961b2ab8cb2b53d01
Mestery <mestery@pm.me>
2021-08-28 20:22:15 +0200

    feat(browser): improve fs and add tests

--- src-browser/fs.js
+++ src-browser/fs.js
@@ -1,55 +1,42 @@
 import memfs from 'memfs';
-import memfsVolume from 'memfs/lib/volume.js';
-const { Volume, createFsFromVolume } = memfs;
+import volume from 'memfs/lib/volume.js';
+const { vol, createFsFromVolume } = memfs;
 import { Buffer } from './buffer.js';
 
 function unimplemented(name) {
   throw new Error(`Node.js fs ${name} is not supported by JSPM core in the browser`);
 }
 
-const vol = new Volume();
+vol.fromNestedJSON({
+  '/dev': { stdin: '', stdout: '', stderr: '' },
+  '/usr/bin': {},
+  '/home': {},
+  '/tmp': {},
+});
 
-vol.StatWatcher.prototype.start = function (path, persistent, interval = 5007) {
-  this.filename = memfsVolume.pathToFilename(path);
-  this.setTimeout = setTimeout.bind(globalThis);
-  this.interval = interval;
-  this.prev = this.vol.statSync(this.filename);
-  this.loop();
-}
-
-// https://github.com/wasmerio/wasmer-js/blob/55fa8c17c56348c312a8bd23c69054b1aa633891/packages/wasmfs/src/index.ts
-vol.mkdirpSync('/dev', 0o777);
-vol.releasedFds = [];
-const files = ['stdin', 'stdout', 'stderr'];
-for (let i = 0; i < 3; i++) {
-  const file = files[i];
-  vol.appendFileSync(`/dev/${file}`, '');
-  vol.releasedFds.push(i);
-  const fd = vol.openSync(`/dev/${file}`, i === 0 ? 'r' : 'w');
-  if (fd !== i) throw new Error(`invalid handle for ${file}: ${fd}`);
-}
-
-// create "real" os paths
-vol.mkdirSync('/usr/bin', { recursive: true });
-vol.mkdirSync('/home');
-vol.mkdirSync('/tmp');
-
+vol.releasedFds = [2, 1, 0];
+vol.openSync('/dev/stdin', 'w');
+vol.openSync('/dev/stdout', 'r');
+vol.openSync('/dev/stderr', 'r');
 watchStdo('/dev/stdout', 1, console.log);
 watchStdo('/dev/stderr', 2, console.error);
-
 function watchStdo(path, fd, listener) {
   let oldSize = 0;
+  const decoder = new TextDecoder();
   vol.watch(path, 'utf8', () => {
-    const { size } = vol.statSync(path);
+    const { size } = vol.fstatSync(fd);
     const buf = Buffer.alloc(size - oldSize);
     vol.readSync(fd, buf, 0, buf.length, oldSize);
     oldSize = size;
-    listener(buf.toString());
+    listener(decoder.decode(buf, { stream: true }));
   });
 }
 
 const fs = createFsFromVolume(vol);
 
+fs.opendir = () => unimplemented('opendir');
+fs.opendirSync = () => unimplemented('opendirSync');
+fs.promises.opendir = () => unimplemented('promises.opendir');
 fs.cp = () => unimplemented('cp');
 fs.cpSync = () => unimplemented('cpSync');
 fs.promises.cp = () => unimplemented('promises.cp');
@@ -60,6 +47,15 @@ fs.rmSync = () => unimplemented('rmSync');
 fs.promises.rm = () => unimplemented('promises.rm');
 fs.Dir = () => unimplemented('Dir');
 fs.promises.watch = () => unimplemented('promises.watch');
+
+// TODO: remove this when https://github.com/streamich/memfs/pull/732 is merged and memfs released
+fs.StatWatcher.prototype.start = function (path, persistent, interval = 5007) {
+  this.filename = volume.pathToFilename(path);
+  this.setTimeout = setTimeout.bind(self);
+  this.interval = interval;
+  this.prev = this.fs.statSync(this.filename);
+  this.loop();
+}
 
 fs.FileReadStream = fs.ReadStream;
 fs.FileWriteStream = fs.WriteStream;
