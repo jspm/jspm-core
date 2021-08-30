8233d4aac561a103234dae309e6a7b497089d87e
Mestery <mestery@pm.me>
2021-08-30 03:00:31 +0200

    Create worker_threads.test.js

---
+++ test/worker_threads.test.js
@@ -0,0 +1,18 @@
+import Test from './common.js';
+import { Worker } from '../nodelibs/browser/worker_threads.js';
+import { once } from '../nodelibs/browser/events.js';
+
+const test = new Test();
+
+const worker = new Worker(`import { parentPort } from '../nodelibs/browser/worker_threads.js';
+import { once } from '../nodelibs/browser/events.js';
+
+parentPort.postMessage('hello');
+parentPort.postMessage(\`received: \${(await once(parentPort, 'message'))[0]}\`);
+`, { eval: true });
+worker.postMessage('hello2');
+
+test.assertObjectEqual(() => await once(worker, 'message'), ['hello']);
+test.assertObjectEqual(() => await once(worker, 'message'), ['received: hello2']);
+
+export default test;
