274e69230ed2aaf1482ca062c836f10f21d7b988
Guy Bedford <guybedford@gmail.com>
2021-09-01 09:08:19 -0700

    use conventional nextTick implementation

--- src-browser/process.js
+++ src-browser/process.js
@@ -2,7 +2,67 @@ function unimplemented(name) {
   throw new Error('Node.js process ' + name + ' is not supported by JSPM core outside of Node.js');
 }
 
-export var nextTick = cb => Promise.resolve().then(cb);
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+  if (!draining || !currentQueue)
+    return;
+  draining = false;
+  if (currentQueue.length) {
+    queue = currentQueue.concat(queue);
+  }
+  else {
+    queueIndex = -1;
+  }
+  if (queue.length)
+    drainQueue();
+}
+
+function drainQueue() {
+  if (draining)
+    return;
+  var timeout = setTimeout(cleanUpNextTick, 0);
+  draining = true;
+
+  var len = queue.length;
+  while(len) {
+    currentQueue = queue;
+    queue = [];
+    while (++queueIndex < len) {
+      if (currentQueue)
+        currentQueue[queueIndex].run();
+    }
+    queueIndex = -1;
+    len = queue.length;
+  }
+  currentQueue = null;
+  draining = false;
+  clearTimeout(timeout);
+}
+
+export function nextTick (fun) {
+  var args = new Array(arguments.length - 1);
+  if (arguments.length > 1) {
+    for (var i = 1; i < arguments.length; i++)
+      args[i - 1] = arguments[i];
+  }
+  queue.push(new Item(fun, args));
+  if (queue.length === 1 && !draining)
+    setTimeout(drainQueue, 0);
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+  this.fun = fun;
+  this.array = array;
+}
+Item.prototype.run = function () {
+  this.fun.apply(null, this.array);
+};
+
 export var title = 'browser';
 export var arch = 'x64';
 export var platform = 'browser';
