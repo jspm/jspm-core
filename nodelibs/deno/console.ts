357a3d9dc9a6de0f18bb8816dc0bc93ee470d536
Guy Bedford <guybedford@gmail.com>
2022-02-01 16:37:51 -0800

    deno update to 0.124.0

--- nodelibs/deno/console.ts
+++ nodelibs/deno/console.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.123.0/node/console.ts';
-export { default } from 'https://deno.land/std@0.123.0/node/console.ts';
+export * from 'https://deno.land/std@0.124.0/node/console.ts';
+export { default } from 'https://deno.land/std@0.124.0/node/console.ts';
--- nodelibs/deno/constants.ts
+++ nodelibs/deno/constants.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.123.0/node/constants.ts';
-export { default } from 'https://deno.land/std@0.123.0/node/constants.ts';
+export * from 'https://deno.land/std@0.124.0/node/constants.ts';
+export { default } from 'https://deno.land/std@0.124.0/node/constants.ts';
