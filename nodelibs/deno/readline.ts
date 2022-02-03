f2b7779b30ca5e31c330cb05237690a3565b0ea6
Guy Bedford <guybedford@gmail.com>
2022-02-03 10:06:36 -0800

    add missing deno updates

--- nodelibs/deno/readline.ts
+++ nodelibs/deno/readline.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.123.0/node/readline.ts';
-export { default } from 'https://deno.land/std@0.123.0/node/readline.ts';
+export * from 'https://deno.land/std@0.125.0/node/readline.ts';
+export { default } from 'https://deno.land/std@0.125.0/node/readline.ts';
--- nodelibs/deno/v8.ts
+++ nodelibs/deno/v8.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.123.0/node/v8.ts';
-export { default } from 'https://deno.land/std@0.123.0/node/v8.ts';
+export * from 'https://deno.land/std@0.125.0/node/v8.ts';
+export { default } from 'https://deno.land/std@0.125.0/node/v8.ts';
