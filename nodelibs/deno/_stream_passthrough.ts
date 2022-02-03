7ed53c708f5b2d796ef5ffe910bd60e902dc35db
Guy Bedford <guybedford@gmail.com>
2022-02-04 01:13:50 +0200

    Update Deno to 0.125.0 (#69)

--- nodelibs/deno/_stream_passthrough.ts
+++ nodelibs/deno/_stream_passthrough.ts
@@ -1,1 +1,1 @@
-export { default } from 'https://deno.land/std@0.124.0/node/_stream/passthrough.ts';
+export { default } from 'https://deno.land/std@0.125.0/node/_stream/passthrough.ts';
--- nodelibs/deno/async_hooks.ts
+++ nodelibs/deno/async_hooks.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.123.0/node/async_hooks.ts';
-export { default } from 'https://deno.land/std@0.123.0/node/async_hooks.ts';
+export * from 'https://deno.land/std@0.125.0/node/async_hooks.ts';
+export { default } from 'https://deno.land/std@0.125.0/node/async_hooks.ts';
